363195b928a7c198eaa2c39527caeb6ad985ef63
Sarah B <sarah.beardsley21@yahoo.com>
2016-05-26 23:55:48 +0000

    various files

--- rpi2.py
+++ rpi2.py
@@ -4,23 +4,35 @@ currently receives AHRS data and outputs data to a file
 """
 
 import datetime
+import decimal
+import serial
 
 # get time program began running
 startTime = datetime.datetime.now()
 
 # lists to store sensor readings
 ahrs = []
+pressure = []
 
 # set up sensors
+ser = serial.Serial('/dev/ttyACM0',57600)
 
 # keep reading values
 count = 0
 while True:
+    # ahrs (dummy vals)
     count += 1
-    # ahrs.append(vectnav.getVals())
     ahrs.append(count)
+    #pressure sensor
+    try:
+        r = ser.readline()
+        r = float(decimal.Decimal(r))
+        pressure.append(r)
+    except:
+        pass
     if count >= 10:
         break
+        
     
 # get time when while loop terminates    
 endTime = datetime.datetime.now()
@@ -32,9 +44,11 @@ outFile = open('output.txt', 'a')
 outFile.write('start: ' + (str(startTime)))
 outFile.write('\nAHRS:\n')
 for val in ahrs:
-    s_val = str(val)
-    outFile.write(s_val)
-    outFile.write('\n')
+    outFile.write(str(val) + '\n')
+
+outFile.write('\nPressure sensor:\n')
+for val in pressure:
+    outFile.write(str(val) + '\n')
 
 outFile.write('\nend: ' + str(endTime))
 outFile.write('\n---------------------------------------------------------------------------------------------------\n')
