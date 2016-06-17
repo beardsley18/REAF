c64b44e9fe584d18f1d7a39caa52442e585ae0e1
Sarah B <pesh_2014@tamu.edu>
2016-06-17 20:02:04 +0000

    ahrs_test debugged

--- VectorNav/examples/vn100_linux_basic/ahrs_test.py
+++ VectorNav/examples/vn100_linux_basic/ahrs_test.py
@@ -1,4 +1,6 @@
 import subprocess
+import threading
+import time
 
 def ahrs():
 	subprocess.call(["./vn100_linux_basic"])
@@ -8,7 +10,28 @@ def count():
 	while(count < 11):
 		print str(count)
 		count += 1
+		time.sleep(1)
 
 
-ahrs()
-count()
+class AhrsThread(threading.Thread):
+	def __init__(self, threadID):
+		threading.Thread.__init__(self)
+        	self.threadID = threadID
+	
+	def run(self):
+		ahrs()
+
+class MainThread(threading.Thread):
+	def __init__(self, threadID):
+		threading.Thread.__init__(self)
+        	self.threadID = threadID
+	
+	def run(self):
+		count()
+
+
+thread1 = AhrsThread(1)
+thread2 = MainThread(2)
+
+thread1.start()
+thread2.start()
