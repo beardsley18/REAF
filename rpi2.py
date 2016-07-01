03bcdd35daaf2cac60ad1055ab839ed50e04099e
Sarah B <pesh_2014@tamu.edu>
2016-07-01 22:53:08 +0000

    debugged thruster calculations

--- rpi2.py
+++ rpi2.py
@@ -1,6 +1,7 @@
 """
 RPi 2 test code
 currently receives AHRS and pressure sensor data and outputs data to a file
+uses pressure sensor data to move Ula 4' below the surface, then move forward
 """
 
 import datetime
@@ -12,15 +13,6 @@ import threading
 
 # get time program began running
 startTime = datetime.datetime.now()
-
-#global variables
-float r = 1.03*10^3 # Density of the fluid (kg/m^3)
-float g = 9.8  # Acceleration due to gravity (m/s^2)
-float patm = 101325  # Atmospheric pressure in pascals (N/m^2)
-float count = 0
-float average = 0
-float ms_per_reading = 100
-float total = 0
 
 # lists to store sensor readings
 ahrs = []
@@ -34,8 +26,18 @@ def ahrsRun():
 	subprocess.call(["./VectorNav/examples/vn100_linux_basic/vn100_linux_basic"])
 
 def mainLoop():
+    test = 0
     time.sleep(1)
     lastRead = 0
+    #constants for pressure sensor calculations
+    # r = 1.03*10^3 # Density of the fluid (kg/m^3)
+    r = 1030
+    g = 9.8  # Acceleration due to gravity (m/s^2)
+    patm = 101325  # Atmospheric pressure in pascals (N/m^2)
+    count = 0
+    average = 0
+    ms_per_reading = 100
+    total = 0
     while True:
         #ahrs
         try:
@@ -48,40 +50,46 @@ def mainLoop():
                 line = ahrsData.readline()
                 lastRead = ahrsData.tell()
         except IOError as e:
-            print e
+            print 'ahrs: ' + str(e)
         #pressure sensor
         try:
-            r = ser.readline()
-            r = float(decimal.Decimal(r))
-            count += count
-            pressure.append(r)
-            # Calculate height of fluid above water
-            h = (ptot-patm)/(r * g)  # Height of the fluid above the object
-            total = total + h
-            average = total / count
+            ptot = ser.readline()
+            ptot = float(decimal.Decimal(ptot))
+            print ptot
+            if (ptot < 25) and (ptot > 14):
+                count += 1
+                pressure.append(ptot)
+                # Calculate height of fluid above water
+                h = (ptot-patm)/(r * g)  # Height of the fluid above the object
+                total = total + h
+                average = total / count
+                print average
 
-            # Send output to thrusters
-            if count == ms_per_reading:
-                while average > 4:
-                    ser.write('b')
-                    if not average > 4:
-                        break
+                # Send output to thrusters
+                if count == ms_per_reading:
+                    while average > 4:
+                        ser.write('b')
+                        if not average > 4:
+                            break
 
-                while average < 4:
-                    ser.write('a')
-                    if not average < 4:
-                        break
+                    while average < 4:
+                        ser.write('a')
+                        if not average < 4:
+                            break
 
-                while average == 4:
-                    ser.write('c')
-                    ser.write('d')
-                    if not average == 4:
-                        break
-               count = 0
-               total = 0
-        except:
-            pass
+                    while average == 4:
+                        ser.write('c')
+                        ser.write('d')
+                        if not average == 4:
+                            break
+                    count = 0
+                    total = 0
+        except Exception as e:
+            print 'pressure/thrusters: ' + str(e)
         time.sleep(1)
+        test += 1
+        if test > 10:
+            break
     ahrsData.close()
         
         
