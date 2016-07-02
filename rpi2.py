64a367860dcacf29a6d0356813b532be034c1cb1
Sarah B <pesh_2014@tamu.edu>
2016-07-02 20:21:44 +0000

    more testing

--- rpi2.py
+++ rpi2.py
@@ -24,6 +24,8 @@ average = 0
 count = 0
 total = 0
 
+test = 0
+
 #constants for pressure sensor calculations
 # r = 1.03*10^3 # Density of the fluid (kg/m^3)
 r = 1030
@@ -33,76 +35,95 @@ ms_per_reading = 100
         
 class AhrsThread(threading.Thread):
 	def __init__(self, threadID):
-		threading.Thread.__init__(self)
-        	self.threadID = threadID
+	    threading.Thread.__init__(self)
+            self.threadID = threadID
 	
 	def run(self):
-        # run C program for getting AHRS data
-		subprocess.call(["./VectorNav/examples/vn100_linux_basic/vn100_linux_basic"])
+            # run C program for getting AHRS data
+            subprocess.call(["./VectorNav/examples/vn100_linux_basic/vn100_linux_basic"])
         
 class ThrustersThread(threading.Thread):
 	def __init__(self, threadID):
-		threading.Thread.__init__(self)
-        	self.threadID = threadID
+	    threading.Thread.__init__(self)
+            self.threadID = threadID
 	
 	def run(self):
-        # Send output to thrusters
-        if count == ms_per_reading:
-            while average > 4:
-                ser.write('b')
+            # Send output to thrusters
+            time.sleep(2)
+            test1 = 0
+            while True:
+                    # print 'thrusters: ' + str(average) + ' ' + str(test)
+                # if count == ms_per_reading:
+                    if average > 4:
+                        ser.write('b')
 
-            while average < 4:
-                ser.write('a')
+                    if average < 4:
+                        ser.write('a')
+			print 'writing to a'
 
-            while average == 4:
-                ser.write('c')
-                ser.write('d')
+                    if average == 4:
+                        ser.write('c')
+                        ser.write('d')
+
+                    test1 += 1
+                    # time.sleep(1)
+                    # if test1 > 10:
+                      #  break
                 
-            count = 0
-            total = 0
+                    # count = 0
+                    # total = 0
 
 class MainThread(threading.Thread):
 	def __init__(self, threadID):
-		threading.Thread.__init__(self)
-        	self.threadID = threadID
+	    threading.Thread.__init__(self)
+            self.threadID = threadID
 	
 	def run(self):
-		test = 0
-        time.sleep(1)
-        lastRead = 0
-        while True:
-            #ahrs
-            try:
-                ahrsData = open('ahrs_output.txt', 'r')
-                ahrsData.seek(lastRead)
-                line = ahrsData.readline()
-                while line != "":
-                    line = line.strip()
-                    ahrs.append(line)
-                    line = ahrsData.readline()
-                lastRead = ahrsData.tell()
-                ahrsData.close()
-            except IOError as e:
-                print 'ahrs: ' + str(e)
-            #get pressure sensor data
-            try:
-                ptot = ser.readline()
-                ptot = float(decimal.Decimal(ptot))
-                if (ptot < 25) and (ptot > 14):
-                    count += 1
-                    pressure.append(ptot)
-                    # Calculate height of fluid above water
-                    h = (ptot-patm)/(r * g)  # Height of the fluid above the object
-                    total = total + h
-                    # threadLock.aquire()
-                    average = total / count
-                    # threadLock.release()
-            except Exception as e:
-                print 'pressure/thrusters: ' + str(e)
+	    test = 0
             time.sleep(1)
-            test += 1
-            if test > 10:
-                break
+            lastRead = 0
+            while True:
+                #ahrs
+                try:
+                    ahrsData = open('ahrs_output.txt', 'r')
+                    ahrsData.seek(lastRead)
+                    line = ahrsData.readline()
+                    while line != "":
+                        line = line.strip()
+                        ahrs.append(line)
+                        line = ahrsData.readline()
+                    lastRead = ahrsData.tell()
+                    ahrsData.close()
+                except IOError as e:
+                    print 'ahrs: ' + str(e)
+                #get pressure sensor data
+                try:
+                    average = 0
+                    count = 0
+                    total = 0
+                    #constants for pressure sensor calculations
+                    # r = 1.03*10^3 # Density of the fluid (kg/m^3)
+                    r = 1030
+                    g = 9.8  # Acceleration due to gravity (m/s^2)
+                    patm = 101325  # Atmospheric pressure in pascals (N/m^2)
+                    ms_per_reading = 100 
+                    ptot = ser.readline()
+                    ptot = float(decimal.Decimal(ptot))
+                    if (ptot < 25) and (ptot > 14):
+                        count += 1
+                        pressure.append(ptot)
+                        # Calculate height of fluid above water
+                        h = (ptot-patm)/(r * g)  # Height of the fluid above the object
+                        total = total + h
+                        # threadLock.aquire()
+                        average = total / count
+                        # threadLock.release()
+                except Exception as e:
+                    print 'pressure: ' + str(e)
+                time.sleep(1)
+                test += 1
+                # if test > 10:
+                  #  break
 
 
 # threadLock = threading.Lock()        
