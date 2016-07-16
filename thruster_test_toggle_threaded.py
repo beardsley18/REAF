770e7821d4670e36a7cff79720038586bb2b2e7e
beardsley18 <sarah.beardsley21@yahoo.com>
2016-07-16 12:34:09 -0500

    next thruster toggling attempt

--- thruster_test_toggle_threaded.py
+++ thruster_test_toggle_threaded.py
@@ -3,10 +3,53 @@ import time
 import random
 import threading
 ser = serial.Serial('/dev/ttyACM0',57600)
+# false if off, true if on
+t1_status = False
+t2_status = False
+t3_status = False
+t4_status = False
+
+t1_stat_changed = False
+t2_stat_changed = False
+t3_stat_changed = False
+t4_stat_changed = False
 
 def send(lett):
+    while True:
         ser.write(lett)
         time.sleep(1)
+        if lett == 'a':
+            if t1_stat_changed:
+                t1_stat_changed = False
+                if t1_status:
+                    t1_status = False
+                else:
+                    t1_status = True
+                break
+        elif lett == 'b':
+            if t2_stat_changed:
+                t2_stat_changed = False
+                if t2_status:
+                    t2_status = False
+                else:
+                    t2_status = True
+                break
+        elif lett == 'c':
+            if t3_stat_changed:
+                t3_stat_changed = False
+                if t3_status:
+                    t3_status = False
+                else:
+                    t3_status = True
+                break
+        elif lett == 'd':
+            if t4_stat_changed:
+                t4_stat_changed = False
+                if t4_status:
+                    t4_status = False
+                else:
+                    t4_status = True
+                break
 
 class readThread(threading.Thread):
 	def __init__(self, threadID):
@@ -16,7 +59,18 @@ class readThread(threading.Thread):
 	def run(self):
             while True:
                 outFile = open('output_pressure_thruster_test.txt', 'a')
-                outFile.write(ser.readline())
+                line = ser.readline()
+                if 'Receiving' in line:
+                    if line[line.length()-1] == 'a':
+                        t1_stat_changed = True
+                    elif line[line.length()-1] == 'b':
+                        t2_stat_changed = True
+                    elif line[line.length()-1] == 'c':
+                        t3_stat_changed = True
+                    elif line[line.length()-1] == 'd':
+                        t4_stat_changed = True
+                # else:
+                outFile.write(line)
                 outFile.close()
 
 class writeThread(threading.Thread):
@@ -25,31 +79,25 @@ class writeThread(threading.Thread):
             self.threadID = threadID
 	
 	def run(self):
-            ser.write('b')
-            ser.write('a')
+            send('b')
             for x in range(0,50000):
-                # time.sleep(.3)
-                # ser.write(random.choice('abcd'))
-                # ser.write('a')
                 print 'Sending a'
                 print x
 
-            ser.write('b')
-            ser.write('a')
+            send('a')
             for x in range(0, 50000):
                 # ser.write('b')
                 print 'Sending b'
                 print x
 
-            ser.write('a')
-            ser.write('b')
+            send('a')
+            send('b')
             for x in range(0, 50000):
                 print 'Sending both'
                 print x
 
-            ser.write('a')
-            ser.write('b')
-            # ser.write('b')
+            send('a')
+            send('b')
     
    
 thread1 = writeThread(1)
